22c6e508d14b61b5b808b863ed548db7d83868e3
Rudi Van Landeghem <rudi.van_landeghem@telenet.be>
2021-03-04 14:47:51 +0100

    commit in branch branch_test

--- git_test/src/git_test/git_test.java
+++ git_test/src/git_test/git_test.java
@@ -2,6 +2,6 @@ package git_test;
 
 public class git_test {
 	public static void main(String[] args) {
-		System.out.println("hello git test");
+		System.out.println("hello git test, from branch branch_test");
 	}
 }
